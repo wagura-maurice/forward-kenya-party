efcc6862f83450629cd0e4bd6b68445f46a92e15
Wagura Maurice <business@waguramaurice.com>
2025-07-10 23:31:08 +0300

    update 1 file and create 1 file

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,18 +1,19 @@
 <!-- resources/js/Components/AuthenticationCard.vue -->
 <template>
     <div
-        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+        class="min-h-screen flex flex-col justify-center items-center bg-gray-100 px-4 py-8 md:h-screen"
     >
         <div
             class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
             <slot name="logo" />
         </div>
-
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg rounded-lg dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-        >
-            <slot />
+        <div class="flex-1 w-full flex flex-col items-center justify-center">
+            <div
+                class="relative flex flex-col w-full max-w-2xl px-8 py-6 bg-white shadow-md overflow-y-auto sm:rounded-lg rounded-lg dark:border dark:bg-gray-800 dark:border-gray-700 h-[90vh]"
+            >
+                <slot />
+            </div>
         </div>
     </div>
 </template>
