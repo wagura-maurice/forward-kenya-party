bc3c8136b633666ae162a5ec193cb09e140e5168
Wagura Maurice <business@waguramaurice.com>
2025-07-10 23:31:16 +0300

    rename 47 files, create 4 files, delete 3 files and update 1 file

--- public/build/assets/InputLabel-CUNjyoLJ.js
+++
@@ -1,1 +0,0 @@
-import{e,o as t,t as o,r as n}from"./app-krI2_M6X.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},u={__name:"InputLabel",props:{value:String},setup(s){return(a,_)=>(t(),e("label",l,[s.value?(t(),e("span",r,o(s.value),1)):(t(),e("span",c,[n(a.$slots,"default")]))]))}};export{u as _};
