bb0bca7c088408f847aff4769f3c88b400499e3f
wagura Maurice <buisness@waguramaurice.com>
2025-07-23 13:26:38 +0300

    last morph

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -182,7 +182,7 @@ onUnmounted(() => {
                             </template>
                             <template v-else>
                                 <a
-                                    :href="route('login')"
+                                    :href="route('register')"
                                     class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                                 >
                                     Join the Movement
@@ -279,7 +279,7 @@ onUnmounted(() => {
                     </template>
                     <template v-else>
                         <a
-                            :href="route('login')"
+                            :href="route('register')"
                             class="block text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                         >
                             Join the Movement
