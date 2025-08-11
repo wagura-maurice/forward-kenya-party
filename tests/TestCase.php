459c18e088d89c14dbd3a0301221acb65d8704e6
wagura Maurice <buisness@waguramaurice.com>
2025-08-11 22:15:55 +0300

    test: update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Ensure we're using the MySQL database for testing
+        if (config('database.default') !== 'mysql') {
+            config(['database.default' => 'mysql']);
+        }
+        
+        // Enable activity logging for tests
+        config(['activitylog.enabled' => true]);
+    }
 }
