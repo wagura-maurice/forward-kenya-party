24e2f3da49da19fa1f1b9ba0ba7a03a8e861b455
wagura Maurice <buisness@waguramaurice.com>
2025-08-11 22:14:55 +0300

    create 10 files, rename 93 files, delete 8 files and update 1 file

---
+++ public/build/assets/SectionTitle-De69onh_.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{j as c,o as n,e as s,r as e}from"./vendor-B5djolF_.js";const a={},i={class:"md:col-span-1 flex justify-between"},r={class:"px-4 sm:px-0"},l={class:"text-lg font-medium text-gray-900"},d={class:"mt-1 text-sm text-gray-600"},_={class:"px-4 sm:px-0"};function m(t,p){return n(),c("div",i,[s("div",r,[s("h3",l,[e(t.$slots,"title")]),s("p",d,[e(t.$slots,"description")])]),s("div",_,[e(t.$slots,"aside")])])}const h=o(a,[["render",m]]);export{h as S};
+//# sourceMappingURL=SectionTitle-De69onh_.js.map
