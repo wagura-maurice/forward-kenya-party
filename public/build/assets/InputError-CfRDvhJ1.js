dc640b7922624e16f019399cef5b50bd548d3d82
wagura Maurice <buisness@waguramaurice.com>
2025-08-26 22:29:00 +0300

    stab

---
+++ public/build/assets/InputError-CfRDvhJ1.js
@@ -0,0 +1,2 @@
+import{A as s,J as t,j as a,o as r,e as o,t as c}from"./vendor-DmCWM84H.js";const n={class:"text-sm text-red-600"},p={__name:"InputError",props:{message:String},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{p as _};
+//# sourceMappingURL=InputError-CfRDvhJ1.js.map
