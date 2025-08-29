89f442c432ea14fa976921799d5d67c982c85999
wagura Maurice <buisness@waguramaurice.com>
2025-08-29 05:11:53 +0300

    update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Activitylog\LogOptions;
 
-class User extends Authenticatable implements MustVerifyEmail
+// class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
