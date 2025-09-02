26669a553056190482f93e824b2dbb00d350fc9c
wagura Maurice <buisness@waguramaurice.com>
2025-09-02 19:17:28 +0300

    update GuestLayout.vue

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -186,10 +186,10 @@ onUnmounted(() => {
                             </template>
                             <template v-else>
                                 <a
-                                    :href="route('register')"
+                                    :href="route('login')"
                                     class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                                 >
-                                    Register
+                                    Sign Up
                                 </a>
                             </template>
                         </div>
@@ -283,10 +283,10 @@ onUnmounted(() => {
                     </template>
                     <template v-else>
                         <a
-                            :href="route('register')"
+                            :href="route('login')"
                             class="block text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                         >
-                            Register
+                            Sign Up
                         </a>
                     </template>
                 </div>
