6a47f2dbeb73acda87f68015f0c104db90fdb45c
wagura Maurice <buisness@waguramaurice.com>
2025-09-02 19:17:00 +0300

    update Show.vue

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -84,9 +84,9 @@ defineProps({
                                     'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300': activeTab !== 'danger'
                                 }"
                                 class="whitespace-nowrap py-4 px-6 border-b-2 font-medium text-sm"
-                                aria-label="Danger Zone"
+                                aria-label="Party Membership"
                             >
-                                <i class="fas fa-exclamation-triangle mr-2"></i>Danger Zone
+                                <i class="fas fa-door-open mr-2"></i>Membership
                             </button>
                         </nav>
                     </div>
@@ -118,7 +118,7 @@ defineProps({
                         </div>
                     </div>
 
-                    <!-- Danger Zone -->
+                    <!-- Membership Management -->
                     <div v-else-if="activeTab === 'danger'" class="p-6">
                         <div v-if="$page.props.jetstream.hasAccountDeletionFeatures">
                             <DeleteUserForm />
