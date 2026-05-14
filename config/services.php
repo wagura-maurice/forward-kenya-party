3516a87ac2fd2dae112e962e03744b51926de3f1
Wagura Maurice <wagura465@gmail.com>
2026-05-15 02:40:24 +0300

    update services.php

--- config/services.php
+++ config/services.php
@@ -35,9 +35,42 @@ return [
         ],
     ],
 
+    'africas_talking' => [
+        'username' => env('AFRICAS_TALKING_USERNAME'),
+        'api_key' => env('AFRICAS_TALKING_API_KEY'),
+        'sender_id' => env('AFRICAS_TALKING_SENDER_ID'),
+        'messaging_endpoint' => env('AFRICAS_TALKING_MESSAGING_ENDPOINT'),
+        'user_endpoint' => env('AFRICAS_TALKING_USER_ENDPOINT'),
+        'balance' => env('AFRICAS_TALKING_BALANCE'),
+    ],
+
+    'mpesa' => [
+        'lnmo_consumer_key' => env('MPESA_LNMO_CONSUMER_KEY'),
+        'lnmo_consumer_secret' => env('MPESA_LNMO_CONSUMER_SECRET'),
+        'lnmo_environment' => env('MPESA_LNMO_ENVIRONMENT'),
+        'lnmo_initiator_password' => env('MPESA_LNMO_INITIATOR_PASSWORD'),
+        'lnmo_initiator_username' => env('MPESA_LNMO_INITIATOR_USERNAME'),
+        'lnmo_pass_key' => env('MPESA_LNMO_PASS_KEY'),
+        'lnmo_short_code' => env('MPESA_LNMO_SHORT_CODE'),
+    ],
+
+    'waha' => [
+        'api_url' => env('WAHA_API_URL'),
+        'api_key' => env('WAHA_API_KEY'),
+        'session' => env('WAHA_SESSION'),
+    ],
+
+    'ippms' => [
+        'base_url' => env('IPPMS_BASE_URL'),
+        'username' => env('IPPMS_USERNAME'),
+        'password' => env('IPPMS_PASSWORD'),
+    ],
+
     'recaptcha' => [
+        'enabled' => env('RECAPTCHA_ENABLED'),
         'site_key' => env('RECAPTCHA_SITE_KEY'),
         'secret_key' => env('RECAPTCHA_SECRET_KEY'),
+        'skip_ip' => env('RECAPTCHA_SKIP_IP'),
     ],
 
 ];
