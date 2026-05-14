d5d05c3eadbacf91acb8b1d70495528fc9b6dc3c
Wagura Maurice <wagura465@gmail.com>
2026-05-15 02:40:33 +0300

    update SettingsTableSeeder.php

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -70,18 +70,18 @@ class SettingsTableSeeder extends Seeder
             5 => 
             array (
                 'id' => 6,
-                'item' => 'MPESA_LNMO_CONSUMER_KEY',
-                'default_value' => 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8',
+                'item' => 'AFRICAS_TALKING_BALANCE',
+                'default_value' => '0',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
-                'created_at' => '2024-07-02 23:48:14',
+                'created_at' => '2024-07-22 14:12:35',
                 'updated_at' => NULL,
             ),
             6 => 
             array (
                 'id' => 7,
-                'item' => 'MPESA_LNMO_CONSUMER_SECRET',
-                'default_value' => 'By9NUqT7NGhzy5Pj',
+                'item' => 'MPESA_LNMO_CONSUMER_KEY',
+                'default_value' => 'uKxU78Y9q2cFruO2fKRWuofRCObzMQh8',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -90,8 +90,8 @@ class SettingsTableSeeder extends Seeder
             7 => 
             array (
                 'id' => 8,
-                'item' => 'MPESA_LNMO_ENVIRONMENT',
-                'default_value' => 'sandbox',
+                'item' => 'MPESA_LNMO_CONSUMER_SECRET',
+                'default_value' => 'By9NUqT7NGhzy5Pj',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -100,8 +100,8 @@ class SettingsTableSeeder extends Seeder
             8 => 
             array (
                 'id' => 9,
-                'item' => 'MPESA_LNMO_INITIATOR_PASSWORD',
-                'default_value' => 'HaVh3tgp',
+                'item' => 'MPESA_LNMO_ENVIRONMENT',
+                'default_value' => 'sandbox',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -110,8 +110,8 @@ class SettingsTableSeeder extends Seeder
             9 => 
             array (
                 'id' => 10,
-                'item' => 'MPESA_LNMO_INITIATOR_USERNAME',
-                'default_value' => 'testapi779',
+                'item' => 'MPESA_LNMO_INITIATOR_PASSWORD',
+                'default_value' => 'HaVh3tgp',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -120,8 +120,8 @@ class SettingsTableSeeder extends Seeder
             10 => 
             array (
                 'id' => 11,
-                'item' => 'MPESA_LNMO_PASS_KEY',
-                'default_value' => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
+                'item' => 'MPESA_LNMO_INITIATOR_USERNAME',
+                'default_value' => 'testapi779',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -130,8 +130,8 @@ class SettingsTableSeeder extends Seeder
             11 => 
             array (
                 'id' => 12,
-                'item' => 'MPESA_LNMO_SHORT_CODE',
-                'default_value' => '174379',
+                'item' => 'MPESA_LNMO_PASS_KEY',
+                'default_value' => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2024-07-02 23:48:14',
@@ -140,11 +140,11 @@ class SettingsTableSeeder extends Seeder
             12 => 
             array (
                 'id' => 13,
-                'item' => 'AFRICAS_TALKING_BALANCE',
-                'default_value' => '0',
+                'item' => 'MPESA_LNMO_SHORT_CODE',
+                'default_value' => '174379',
                 'current_value' => NULL,
                 'deleted_at' => NULL,
-                'created_at' => '2024-07-22 14:12:35',
+                'created_at' => '2024-07-02 23:48:14',
                 'updated_at' => NULL,
             ),
             13 => 
@@ -175,6 +175,76 @@ class SettingsTableSeeder extends Seeder
                 'current_value' => NULL,
                 'deleted_at' => NULL,
                 'created_at' => '2025-05-12 00:00:00',
+                'updated_at' => NULL,
+            ),
+            16 => 
+            array (
+                'id' => 17,
+                'item' => 'IPPMS_BASE_URL',
+                'default_value' => 'https://api-ippms-uat.orpp.or.ke',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            17 => 
+            array (
+                'id' => 18,
+                'item' => 'IPPMS_USERNAME',
+                'default_value' => 'wagura465@gmail.com',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            18 => 
+            array (
+                'id' => 19,
+                'item' => 'IPPMS_PASSWORD',
+                'default_value' => 'FKP@Kenya872',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            19 => 
+            array (
+                'id' => 20,
+                'item' => 'RECAPTCHA_ENABLED',
+                'default_value' => 'true',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            20 => 
+            array (
+                'id' => 21,
+                'item' => 'RECAPTCHA_SITE_KEY',
+                'default_value' => '6Lclb7YrAAAAAH1ZmtSFhBpYFspgpDe2rOlLmnF9',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            21 => 
+            array (
+                'id' => 22,
+                'item' => 'RECAPTCHA_SECRET_KEY',
+                'default_value' => '6Lclb7YrAAAAAJpCovLB70HijtOO7oAhD_M-6RU_',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
+                'updated_at' => NULL,
+            ),
+            22 => 
+            array (
+                'id' => 23,
+                'item' => 'RECAPTCHA_SKIP_IP',
+                'default_value' => '',
+                'current_value' => NULL,
+                'deleted_at' => NULL,
+                'created_at' => '2025-05-15 00:00:00',
                 'updated_at' => NULL,
             ),
         ));
